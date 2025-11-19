fbd01ad0db9a5277aaf5f8155c4f8a22687c0609
Lukas Friis <lukasfriis2003@hotmail.com>
2025-11-19 12:17:05 +0100

    Added: User class and IPrivileges with all their relations

---
+++ project/User.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        private int SSN { get; set; }
+        public List<IPrivileges> PrivilegeList{ get; set; }
+        public IPrivileges Privileges { get; set; }
+    }
+}
