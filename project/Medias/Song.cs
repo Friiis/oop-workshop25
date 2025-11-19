a1970b5374b5db4182c3009c2efc712acfeb3043
Lukas Friis <lukasfriis2003@hotmail.com>
2025-11-19 13:09:47 +0100

    fixed errors

--- project/Medias/Song.cs
+++ project/Medias/Song.cs
@@ -5,5 +5,5 @@ class Song //abstract
     public string Composer { get; set; }
     public string singer { get; set; }
     public Language language { get; set; }
-    public Genre genre
+    public SongGenre genre;
 }
--- project/Medias/VideoGames.cs
+++ project/Medias/VideoGames.cs
@@ -4,6 +4,6 @@ class VideoGame //abstract
 {
 	public string Publisher { get; set; }
 	public GameGenre Genre { get; set; }
-    public list <SupportedPlatform> SupportedPlatforms { get; set; }
+    //public list<SupportedPlatform> SupportedPlatforms { get; set; }
 
 }
