42cdc6dcc7fe67e38051204bf41ab141b7389bbd
Lukas Friis <lukasfriis2003@hotmail.com>
2025-11-19 12:40:19 +0100

    Moved: Main method from System to a new MainClass in the Presentation namespace

--- project/Privileges/AddMedia.cs
+++ project/Privileges/AddMedia.cs
@@ -8,5 +8,6 @@ namespace Domain
 {
     internal class AddMedia : IPrivileges
     {
+
     }
 }
--- project/System.cs
+++ project/System.cs
@@ -2,14 +2,5 @@
 
 class System
 {
-    static bool programRunning = true;
-    static void Main(string[] args)
-    {
-        while (programRunning)
-        {
-            Console.Write("> ");
-            string? Cmd = Console.ReadLine();
-            Console.WriteLine(Cmd);
-        }
-    }
+
 }
