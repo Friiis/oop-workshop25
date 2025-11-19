566411870574c1b9b32831f50fac678dbb213694
ElminP <elpit25@student.sdu.dk>
2025-11-19 13:02:55 +0100

    Added: enums folder & Attributes with Song & VideoGames

---
+++ project/enums/Genre.cs
@@ -0,0 +1,25 @@
+namespace Domain
+
+public enum SongGenre
+{
+	Pop,
+	Rock,
+	Jazz,
+	Classical,
+	HipHop,
+	Country,
+	Reggae,
+	Blues,
+	Electronic,
+	Folk
+}
+
+public enum GameGenre
+{
+	Action,
+	Adventure,
+	RolePlaying,
+	Simulation,
+	Strategy,
+	Sports
+}
